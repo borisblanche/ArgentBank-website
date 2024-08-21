0ff5de0d4e5d7493c7f50ed2edd920369fcd1cc7
boris blanche <borisblanche77gmail.com>
2024-08-21 18:56:32 +0200

    Projet terminé

--- ArgentBank-app/argentbank-app/src/components/user-createAt/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function CreatedAt({ updatedAt }) {
-  return (
-    <p>Crée le : {updatedAt}</p>
-  );
-}
-
-export default CreatedAt;
--- ArgentBank-app/argentbank-app/src/components/user-email/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Email({ email }) {
-  return (
-    <p>E-mail: {email}</p>
-  );
-}
-
-export default Email;
